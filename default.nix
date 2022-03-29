ba4c9733663a1709b5f6f2520b2f7cb3980f0534
Someone Serge <sergei.kozlukov@aalto.fi>
2022-03-29 05:30:18 +0300

    fix: bad to call to tryEval wrapper
    ...apparently lead to evaluation of store paths for entire nixpkgs?

--- default.nix
+++ default.nix
@@ -5,36 +5,19 @@
 }:
 let
   trace = if debug then builtins.trace else (msg: value: value);
-  # Tweaked version of nixpkgs/maintainers/scripts/check-hydra-by-maintainer.nix
-  #
-  # It traverses nixpkgs recursively, respecting recurseForDerivations and 
-  # returns a list of name/value pairs of all the packages matching "cond"
-  packagesWith = prefix: cond: set:
-    lib.flatten
-      (lib.mapAttrsToList
-        (key: v:
-          let
-            name = "${prefix}${key}";
-            result = builtins.tryEval
-              (
-                if lib.isDerivation v && cond name v then
-                # Skip packages whose closure fails on evaluation.
-                # This happens for pkgs like `python27Packages.djangoql`
-                # that have disabled Python pkgs as dependencies.
-                  builtins.seq v.outPath [ (lib.nameValuePair name v) ]
-                else if v.recurseForDerivations or false || v.recurseForRelease or false
-                # Recurse
-                then packagesWith "${name}_" cond v
-                else [ ]
-              );
-          in
-          if result.success
-          then trace name result.value
-          else [ ]
-        )
-        set
-      )
-  ;
+
+  # cf. Tweaked version of nixpkgs/maintainers/scripts/check-hydra-by-maintainer.nix
+  maybeBuildable = v:
+    let result = builtins.tryEval
+      (
+        if lib.isDerivation v then
+        # Skip packages whose closure fails on evaluation.
+        # This happens for pkgs like `python27Packages.djangoql`
+        # that have disabled Python pkgs as dependencies.
+          builtins.seq v.outPath [ v ]
+        else [ ]
+      );
+    in if result.success then result.value else [ ];
 
   isUnfreeRedistributable = licenses:
     lib.lists.any (l: (!l.free or true) && (l.redistributable or false)) licenses;
@@ -50,7 +33,6 @@ let
   nixpkgsInstances = lib.mapAttrs
     (configName: config: import inputs.nixpkgs ({ inherit system; } // config))
     configs;
-  supportedPackages = lib.mapAttrs (cfgName: pkgs: packagesWith "" (_: _: true) pkgs) nixpkgsInstances;
 
   extraPackages = [
     [ "blas" ]
@@ -93,23 +75,20 @@ let
           cfg = builtins.attrNames configs;
           path = extraPackages ++ pythonAttrs;
         };
-      maybeSupported = builtins.map
+      supported = builtins.concatMap
         ({ cfg, path }:
           let
             jobName = lib.concatStringsSep "_" ([ cfg ] ++ path);
-            mbPackage = lib.attrByPath path [ ] supportedPackages.${cfg};
+            package = lib.attrByPath path [ ] nixpkgsInstances.${cfg};
+            mbSupported = maybeBuildable package;
           in
-          { inherit jobName mbPackage; })
-        matrix;
-      nonempty = builtins.concatMap
-        ({ jobName, mbPackage }:
-          if mbPackage == [ ]
+          if mbSupported == [ ]
           then [ ]
-          else [{ inherit jobName; package = builtins.head mbPackage; }])
-        maybeSupported;
+          else [{ inherit jobName; package = (builtins.head mbSupported); }])
+        matrix;
       kvPairs = builtins.map
-        ({ jobName, mbPackage }: lib.nameValuePair jobName mbPackage)
-        nonempty;
+        ({ jobName, package }: lib.nameValuePair jobName package)
+        supported;
     in
     lib.listToAttrs kvPairs;
 in
