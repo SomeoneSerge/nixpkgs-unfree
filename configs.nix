2e659626394c4ae44b585480a5d40776d8e95f46
Someone Serge <sergei.kozlukov@aalto.fi>
2022-04-10 23:32:12 +0300

    merge the overlays from configs
    ...also apply perf overrides by default,
    except for intel stuff

--- configs.nix
+++ configs.nix
@@ -1,28 +1,16 @@
-{
-  vanilla = {
-    config.allowUnfree = true;
-    config.cudaSupport = true;
-    overlays = [ ];
-  };
-
-  mklCuda11 =
+let
+  prepareOverlay =
+    { isIntel ? false
+    , cudaVersion ? null
+    , cudnnVersion ? null
+    }:
+    final: prev:
     let
-      potentialNames = {
-        cudnn = [ "cudnn_cudatoolkit_11" "cudnn_8_3_cudatoolkit_11" ];
-      };
-
-      tryByName = pkgs: name: if builtins.hasAttr name pkgs then [ pkgs.${name} ] else [ ];
-      byAnyName = pkgs: name: builtins.head (pkgs.lib.concatMap (tryByName pkgs) potentialNames.${name});
-
-      overlay = final: prev: {
-        cudatoolkit = final.cudatoolkit_11;
-        cudnn = byAnyName final "cudnn";
-        cutensor = final.cutensor_cudatoolkit_11;
-
-        mpich = prev.mpich.override {
-          ch4backend = final.ucx;
-        };
-
+      inherit (prev.lib) optionalAttrs versionOlder replaceChars;
+    in
+    (
+      {
+        # These ignore config.cudaSupport in some releases
         openmpi = prev.openmpi.override {
           cudaSupport = true;
         };
@@ -35,6 +23,11 @@
           enableCuda = true;
         };
 
+        mpich = prev.mpich.override {
+          ch4backend = final.ucx;
+        };
+
+      } // optionalAttrs isIntel {
         blas = prev.blas.override {
           blasProvider = final.mkl;
         };
@@ -42,12 +35,54 @@
         lapack = prev.lapack.override {
           lapackProvider = final.mkl;
         };
-      };
-    in
-    {
-      config.allowUnfree = true;
-      config.cudaSupport = true;
 
-      overlays = [ overlay ];
-    };
+        # TODO: opencv: enable TBB
+      } //
+      (
+        let
+          dontOverride = builtins.isNull cudaVersion && builtins.isNull cudnnVersion;
+
+          versionToAttr = v: if builtins.isNull v then "" else "_${replaceChars ["."] ["_"] v}";
+          cudaAttr = versionToAttr cudaVersion;
+          cudnnAttr = versionToAttr cudnnVersion;
+
+          overlays."21.11" =
+            assert (builtins.isNull cudnnVersion || cudnnVersion == final.cudnn.version);
+            {
+              cudatoolkit_11 = final."cudatoolkit${cudaAttr}";
+              cudatoolkit = final."cudatoolkit${cudaAttr}";
+              cudnn = final."cudnn_cudatoolkit${cudaAttr}";
+              cutensor = final."cutensor_cudatoolkit${cudaAttr}";
+            };
+          overlays."22.05" =
+            {
+              # Assuming Fridh's PR has been merged
+              cudaPackages = prev."cudaPackages${cudaAttr}".overrideScope' (final: prev: {
+                cudnn = final."cudnn${cudnnAttr}";
+              });
+            };
+
+          release = final.lib.nixpkgsVersion;
+          overlay =
+            if versionOlder release "21.12" then "21.11"
+            else if versionOlder release "22.06" then "22.05"
+            else throw "Unsuported nixpkgs release: ${release}";
+        in
+        optionalAttrs (!dontOverride) overlays.${overlay}
+      )
+    );
+in
+{
+  vanilla = {
+    config.allowUnfree = true;
+    config.cudaSupport = true;
+
+    overlays = [ (prepareOverlay { }) ];
+  };
+  intel = {
+    config.allowUnfree = true;
+    config.cudaSupport = true;
+
+    overlays = [ (prepareOverlay { isIntel = true; }) ];
+  };
 }
