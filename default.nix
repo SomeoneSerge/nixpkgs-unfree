7c716ccef51332e90777589c53265a09a3c0fbfa
Someone Serge <sergei.kozlukov@aalto.fi>
2022-04-19 21:46:11 +0300

    default.nix: make it a proxy function
    - allows to `import <nixpkgs> { ... }` (previous version only allowed
      `import <nixpkgs>`, which was inconsistent)
    - `import <nixpkgs> { }` should evaluate nixpkgs with cudaSupport=true;
    - `import <nixpkgs> { config = { }; }` should evaluate vanilla nixpkgs

--- default.nix
+++ default.nix
@@ -1,6 +1,16 @@
 let
+  defaultConfig = { allowUnfree = true; cudaSupport = true; };
+in
+
+{ config ? defaultConfig, ... }@userArgs:
+
+let
   flake = builtins.getFlake (builtins.toString ./.);
   system = builtins.currentSystem;
-  output = flake.legacyPackages.${builtins.currentSystem};
+
+  args = userArgs // { inherit config; };
+  newInstance = import flake.inputs.nixpkgs args;
+  defaultOutput = flake.legacyPackages.${builtins.currentSystem};
+  spawn1000Instances = !(args == { config = defaultConfig; });
 in
-output
+if spawn1000Instances then newInstance else defaultOutput
