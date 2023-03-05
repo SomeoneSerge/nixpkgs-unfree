977b52faa14c6ff5d426e37332262d91cc814224
Someone Serge <sergei.kozlukov@aalto.fi>
2023-03-05 13:19:21 +0200

    fixup! flake.nix: try flake-update effect

--- flake.nix
+++ flake.nix
@@ -3,10 +3,13 @@
 
   inputs = {
     nixpkgs.follows = "nixpkgs-master";
+
     nixpkgs-master.url = github:NixOS/nixpkgs/master;
     nixpkgs-nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     nixpkgs-nixos-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
     nixpkgs-release.url = github:NixOS/nixpkgs/nixos-21.11;
+
+    hercules-ci-effects.url = github:hercules-ci/hercules-ci-effects;
   };
 
   nixConfig = {
@@ -18,118 +21,130 @@
     ];
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
-    let
-
-      inherit (nixpkgs) lib;
-
-      systems = [ "x86_64-linux" ];
-
-      eachSystem = lib.genAttrs systems;
-
-      utils = import ./nix/utils.nix;
-      inherit (utils) optionalString;
-
-      x = eachSystem (system:
-        import ./nix/jobs.nix {
-          inherit system nixpkgs lib;
-        }
-      );
-    in
-    {
-      # Inherit from upstream
-      inherit (nixpkgs) lib; # nixosModules htmlDocs;
-
-      # But replace legacyPackages with the unfree version
-      legacyPackages = eachSystem (system: x.${system}.legacyPackages);
-
-      # And load all the unfree+redistributable packages as checks
-      checks = eachSystem (system: x.${system}.neverBreak);
-
-      # Expose our own unfree overrides
-      overlays = import ./nix/overlays.nix;
-      overlay = self.overlays.basic;
-
-      herculesCI =
-        { ref
-        , branch
-        , tag
-        , rev
-        , shortRev
-        , primaryRepo
-        , herculesCI
-        }: {
-          onPush = { };
-          # onPush.default.outputs = {
-          #   defaultChecks = self.checks;
-          #   neverBreak = x.x86_64-linux.neverBreak;
-          # };
-
-          # Cf. https://docs.hercules-ci.com/hercules-ci-agent/evaluation#attributes-herculesCI.onSchedule-when
-          onSchedule.neverBreak = {
-            when.hour = [ 0 2 20 22 ];
-            outputs =
-              let
-                system = "x86_64-linux";
-                cudaCapabilities = [ "8.6" ];
-                jobs = import ./nix/jobs.nix {
-                  inherit system lib;
-                  nixpkgs = inputs.nixpkgs-master;
-                  extraConfig = { inherit cudaCapabilities; };
-                };
-              in
-              jobs.neverBreak;
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        imports = [
+          inputs.hercules-ci-effects.flakeModule
+        ];
+        systems = [ "x86_64-linux" ];
+        hercules-ci.flake-update = {
+          enable = true;
+          when = {
+            minute = 30;
+            hour = [ 2 19 ];
           };
-
-          onSchedule.masterAmpere = {
-            when.hour = [ 21 ];
-            outputs =
-              let
-                system = "x86_64-linux";
-                cudaCapabilities = [ "8.6" ];
-                jobs = import ./nix/jobs.nix {
-                  inherit system lib;
-                  nixpkgs = inputs.nixpkgs-master;
-                  extraConfig = { inherit cudaCapabilities; };
-                };
-              in
-              jobs.checks;
-          };
-
-          # Default cudaCapabilities
-
-          onSchedule.nixpkgsUnstableMatrix = {
-            when.dayOfWeek = [ "Sat" ];
-            outputs =
-              let
-                system = "x86_64-linux";
-                jobs = import ./nix/jobs.nix {
-                  inherit system lib;
-                  nixpkgs = inputs.nixpkgs-nixpkgs-unstable;
-                };
-              in
-              jobs.checks;
-          };
-          onSchedule.nixosUnstableMatrix = {
-            when.dayOfWeek = [ "Sat" ];
-            outputs =
-              let
-                system = "x86_64-linux";
-                jobs = import ./nix/jobs.nix {
-                  inherit system lib;
-                  nixpkgs = inputs.nixpkgs-nixos-unstable;
-                };
-              in
-              jobs.checks;
-          };
+          autoMergeMethod = "rebase";
+          createPullRequest = false;
         };
+        flake =
+          let
 
-      # Not sure if it goes here, or if I need flake-parts
-      flake-update = {
-        enable = true;
-        when = {
-          hour = [ 2 20 ];
-        };
+            inherit (inputs) nixpkgs;
+            inherit (nixpkgs) lib;
+
+            systems = [ "x86_64-linux" ];
+
+            eachSystem = lib.genAttrs systems;
+
+            utils = import ./nix/utils.nix;
+            inherit (utils) optionalString;
+
+            x = eachSystem (system:
+              import ./nix/jobs.nix {
+                inherit system nixpkgs lib;
+              }
+            );
+          in
+          {
+            # Inherit from upstream
+            inherit (nixpkgs) lib; # nixosModules htmlDocs;
+
+            # But replace legacyPackages with the unfree version
+            legacyPackages = eachSystem (system: x.${system}.legacyPackages);
+
+            # And load all the unfree+redistributable packages as checks
+            checks = eachSystem (system: x.${system}.neverBreak);
+
+            # Expose our own unfree overrides
+            overlays = import ./nix/overlays.nix;
+            overlay = self.overlays.basic;
+          };
+
+        herculesCI =
+          { ref
+          , branch
+          , tag
+          , rev
+          , shortRev
+          , primaryRepo
+          , herculesCI
+          , lib # Because of flake-parts...
+          , ...
+          }: {
+            onPush = lib.mkForce { };
+            # onPush.default.outputs = {
+            #   defaultChecks = self.checks;
+            #   neverBreak = x.x86_64-linux.neverBreak;
+            # };
+
+            # Cf. https://docs.hercules-ci.com/hercules-ci-agent/evaluation#attributes-herculesCI.onSchedule-when
+            onSchedule.neverBreak = {
+              when.hour = [ 0 2 20 22 ];
+              outputs =
+                let
+                  system = "x86_64-linux";
+                  cudaCapabilities = [ "8.6" ];
+                  jobs = import ./nix/jobs.nix {
+                    inherit system;
+                    nixpkgs = inputs.nixpkgs-master;
+                    extraConfig = { inherit cudaCapabilities; };
+                  };
+                in
+                jobs.neverBreak;
+            };
+
+            onSchedule.masterAmpere = {
+              when.hour = [ 21 ];
+              outputs =
+                let
+                  system = "x86_64-linux";
+                  cudaCapabilities = [ "8.6" ];
+                  jobs = import ./nix/jobs.nix {
+                    inherit system;
+                    nixpkgs = inputs.nixpkgs-master;
+                    extraConfig = { inherit cudaCapabilities; };
+                  };
+                in
+                jobs.checks;
+            };
+
+            # Default cudaCapabilities
+
+            onSchedule.nixpkgsUnstableMatrix = {
+              when.dayOfWeek = [ "Sat" ];
+              outputs =
+                let
+                  system = "x86_64-linux";
+                  jobs = import ./nix/jobs.nix {
+                    inherit system;
+                    nixpkgs = inputs.nixpkgs-nixpkgs-unstable;
+                  };
+                in
+                jobs.checks;
+            };
+            onSchedule.nixosUnstableMatrix = {
+              when.dayOfWeek = [ "Sat" ];
+              outputs =
+                let
+                  system = "x86_64-linux";
+                  jobs = import ./nix/jobs.nix {
+                    inherit system;
+                    nixpkgs = inputs.nixpkgs-nixos-unstable;
+                  };
+                in
+                jobs.checks;
+            };
+          };
       };
-    };
 }
