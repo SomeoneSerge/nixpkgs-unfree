d1f310cb0ddc37f456193fa74ad8ad151c704c59
Someone Serge <sergei.kozlukov@aalto.fi>
2023-11-26 20:14:51 +0000

    nixos-release: 23.05 -> 23.11

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixpkgs-master.url = github:NixOS/nixpkgs/master;
     nixpkgs-nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     nixpkgs-nixos-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
-    nixpkgs-release.url = github:NixOS/nixpkgs/nixos-23.05;
+    nixpkgs-release.url = github:NixOS/nixpkgs/nixos-23.11;
 
     hercules-ci-effects = {
       url = "github:hercules-ci/hercules-ci-effects";
